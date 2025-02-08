c58734f1ee0273ab8bbbee111a8c7561a2dc4a28
SRTareq <sajedurtareq@gmail.com>
2025-02-09 02:30:48 +0600

    gallery  facebook icon and address

--- js/publicationscript.js
+++ js/publicationscript.js
@@ -24,3 +24,21 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 });
+
+////galleru script////
+
+let loadMoreBtn = document.querySelector('#load-more');
+let currentItem = 3;
+
+loadMoreBtn.onclick = () => {
+    let boxes = [...document.querySelectorAll('.container .box-container .box')];
+    for (var i = currentItem; i < currentItem + 3; i++) {
+        boxes[i].style.display = 'inline-block';
+    }
+    currentItem += 3;
+
+    if (currentItem >= boxes.length) {
+        loadMoreBtn.style.display = 'none';
+    }
+}
+
