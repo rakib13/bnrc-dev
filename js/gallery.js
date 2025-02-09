c60153a728095a6b46b455eb42f54fd2bed41ef0
SRTareq <sajedurtareq@gmail.com>
2025-02-10 01:05:48 +0600

    galley agin commit korchi

--- js/gallery.js
+++ js/gallery.js
@@ -1,16 +1,14 @@
-
-
 let loadMoreBtn = document.querySelector('#load-more');
+let boxes = document.querySelectorAll('.grid-container .box');
 let currentItem = 3;
 
 loadMoreBtn.onclick = () => {
-    let boxes = [...document.querySelectorAll('.container .box-container .box')];
-    for (var i = currentItem; i < currentItem + 3; i++) {
-        boxes[i].style.display = 'inline-block';
+    for (let i = currentItem; i < currentItem + 3 && i < boxes.length; i++) {
+        boxes[i].style.display = 'block'; // Show more boxes
     }
     currentItem += 3;
 
     if (currentItem >= boxes.length) {
-        loadMoreBtn.style.display = 'none';
+        loadMoreBtn.style.display = 'none'; // Hide button if no more items
     }
-}
+};
